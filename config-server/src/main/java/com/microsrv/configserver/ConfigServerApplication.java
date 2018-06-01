e17b11d98a5020e02eeb2b755559a14ed6cba5c1
yu.sun <m_suny@163.com>
2018-06-01 14:48:56 +0800

    sunyu::update::update log4j config

--- config-server/src/main/java/com/microsrv/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/microsrv/configserver/ConfigServerApplication.java
@@ -1,14 +1,20 @@
 package com.microsrv.configserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
 @EnableEurekaServer
 public class ConfigServerApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(ConfigServerApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("这里是服务注册中心:{}",ConfigServerApplication.logger.getName());
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 }
